9ffe5286c29fe926faf54c03947bdfc2267bad53
leon-admin <leon.oganiesjan@gmail.com>
2026-01-01 15:39:36 +0100

    Added vars for static site module

---
+++ modules/static_site/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev, prod)"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
