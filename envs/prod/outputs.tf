163eaa318906e9542a4873ad22380e3ea3c7c227
leon-admin <leon.oganiesjan@gmail.com>
2026-01-01 17:43:47 +0100

    Added cloudfron distribution id to module output for proper invalidation support

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -5,3 +5,7 @@ output "cloudfront_url" {
 output "bucket_name" {
   value = module.static_site.bucket_name
 }
+
+output "cloudfront_distribution_id" {
+  value = module.static_site.cloudfront_distribution_id
+}
--- modules/static_site/outputs.tf
+++ modules/static_site/outputs.tf
@@ -7,3 +7,8 @@ output "cloudfront_url" {
   value       = "https://${aws_cloudfront_distribution.cdn.domain_name}"
   description = "CloudFront distribution URL"
 }
+
+output "cloudfront_distribution_id" {
+  value       = aws_cloudfront_distribution.cdn.id
+  description = "CloudFront distribution ID"
+}
